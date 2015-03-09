4bc8fe512c8564e0b2309bb95743a40f39584a37
Alex Bradaric <alex@bradaric.com>
2015-03-09 11:37:49 +0100

    Adds ll_intercom_show_widget filter

--- intercom-for-wordpress.php
+++ intercom-for-wordpress.php
@@ -294,7 +294,14 @@ class ll_intercom {
 		$out .= '// Intercom for WordPress | v' . LL_INTERCOM_VERSION . ' | https://wordpress.org/plugins/intercom-for-wordpress' . "\n";
 		$out .= 'window.intercomSettings = ' . $settings_json . ';' . "\n";
 		$out .= '</script>' . "\n";
-		$out .= '<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic(\'reattach_activator\');ic(\'update\',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement(\'script\');s.type=\'text/javascript\';s.async=true;s.src=\'https://widget.intercom.io/widget/' . $opts[ 'app-id' ] . '\';var x=d.getElementsByTagName(\'script\')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent(\'onload\',l);}else{w.addEventListener(\'load\',l,false);}}})()</script>' . "\n";
+
+		// allowing plugins/themes to hide just the widget
+
+		$out .= '<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic(\'reattach_activator\');ic(\'update\',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;';
+		if ( apply_filters( 'll_intercom_show_widget', true ) ) {
+			$out .=  'function l(){var s=d.createElement(\'script\');s.type=\'text/javascript\';s.async=true;s.src=\'https://widget.intercom.io/widget/' . $opts[ 'app-id' ] . '\';var x=d.getElementsByTagName(\'script\')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent(\'onload\',l);}else{w.addEventListener(\'load\',l,false);}';
+		}
+		$out .=  '}})()</script>' . "\n";
 
 		echo $out;
 
